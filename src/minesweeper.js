895162612c40d70083cce6b220f56201280690c9
Steve Herrmann <37193114+Herrm2291@users.noreply.github.com>
2018-04-28 14:23:00 -0400

    added code to get the number of neighbor bombs and for a user to flip a tile

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -25,9 +25,47 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
       board[randomRowIndex][randomColumnIndex] = 'B';
       numberOfBombsPlaced++
     }
-    board[randomRowIndex][randomColumnIndex] = 'B';
-    numberOfBombsPlaced++
   } return board;
+};
+
+const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
+  const neighborOffsets = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1]
+  ];
+
+  const numberOfRows = bombBoard.length;
+  const numberOfColumns = bombBoard[0].length;
+  let numberOfBombs = 0;
+
+  neighborOffsets.forEach(offset => {
+    const neighborRowIndex = rowIndex + offset[0];
+    const neightborColumnIndex = columnIndex + offset[1];
+    if (neighborRowIndex >= 0 &&
+        neighborRowIndex < numberOfRows &&
+        neightborColumnIndex >= 0 &&
+        neightborColumnIndex < numberOfColumns) {
+      if (bombBoard[neighborRowIndex][neightborColumnIndex] === 'B') {
+        numberOfBombs++;
+      }
+    }
+  });
+  return numberOfBombs;
+};
+
+const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
+  if (playerBoard[rowIndex][columnIndex] !== ' ') {
+    console.log('This tile has already been flipped!');
+    return;
+  } else if (bombBoard[rowIndex][columnIndex] === 'B') {
+      bombBoard[rowIndex][columnIndex] = 'B';
+  }
 };
 
 const printBoard = board => {
