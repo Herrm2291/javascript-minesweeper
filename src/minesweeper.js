4370ad88d72077f3bdb45bbb37a5e24ef807b31f
Steve Herrmann <37193114+Herrm2291@users.noreply.github.com>
2018-04-27 20:01:19 -0400

    added code to dynamically generate the bomb board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -8,8 +8,34 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   } return board;
 };
 
-// console.log(generatePlayerBoard(2, 3));
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  const board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    let row = [];
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(null);
+    } board.push(row);
+  }
 
-const generateBombBoard = () => {
-  
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+    // Bombs currently have the potential to be placed on top of other bombs which will be fixed later
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++
+  } return board;
+};
+
+const printBoard = board => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
 }
+
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
+
